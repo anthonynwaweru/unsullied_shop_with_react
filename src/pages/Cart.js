0d07e0d698c86daa466de03f83aa9873130cb52f
anthonypmarcel <anthonypmarcel@gmail.com>
2022-08-29 19:39:25 +0300

    cart update

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,13 +5,44 @@ import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 20px;
+`;
 
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const TopButton = styled.button`
+  padding: 10px;
+  cursor: pointer;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const BottomButton = styled.button``;
 const Cart = () => {
   return (
     <Container>
       <Navbar />
       <Announcements />
-      Cart
+      <Wrapper>
+        <Title>Your Cart</Title>
+        <Top>
+          <TopButton>Top</TopButton>
+        </Top>
+        <Bottom>
+          <BottomButton>Bottom</BottomButton>
+        </Bottom>
+      </Wrapper>
       <Footer />
     </Container>
   );
